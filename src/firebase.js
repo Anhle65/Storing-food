2aa33fe6ed95b219a7239e561bda392d12fe9b4a
Anhle65 <levananh235813@gmail.com>
2025-10-23 23:27:04 +1300

    Refactor: use .env for Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyB6NBVMcCS-V8Ly-OR3Yres3KJ5k9aXXHA",
+    apiKey: process.env.apiKey,
     authDomain: "food-tracker-storage-33635.firebaseapp.com",
     projectId: "food-tracker-storage-33635",
-    storageBucket: "food-tracker-storage-33635.firebasestorage.app",
+    storageBucket: process.env.storageBucket,
     messagingSenderId: "393831960963",
     appId: "1:393831960963:web:2f21d8bc3abb905e2be6b6",
-    measurementId: "G-3KX1K6P9S7"
+    measurementId: process.env.measurementId
 };
 
 // Initialize Firebase
